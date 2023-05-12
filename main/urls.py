d8cdd9f9df374db1c598dee120e1c0bedfcaeada
alinasabirova <alinasabirova46@gmail.com>
2023-05-12 12:22:45 +0500

    в принципе рабочий вариант коммит перед проверкой эффективности

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = {
     path('', views.index),
     path('supermag', views.download_data_from_sm),
+    path('kriging', views.kriging),
 }
--- main/views.py
+++ main/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import pandas as pd
+from pandas_geojson import to_geojson
 
-
+coord = []
+param = []
 def download_data_from_sm(request):
-    import supermag as sm
-    import pandas as pd
-    from pandas_geojson import to_geojson
-    # start = [2012, 10, 15, 11, 00, 00]
+    import supermagApi as sm
+    from datetime import datetime
+    import time
+
+    start_time = datetime.now()
+    # start = '1974-12-12T12:00'
     start = request.GET.get('dt')
     # print(new_coord)
 
@@ -15,33 +20,30 @@ def download_data_from_sm(request):
     print(len(stations))
 
     list = []
-    coord = []
-    param = []
 
     for i in range(len(stations)):
         (status, data) = sm.SuperMAGGetData('alina', start, 60, 'ALL', stations[i])
-        s = stations[i], data.glat.to_string(index=False), data.glon.to_string(index=False), data.sza.to_string(
-            index=False)
+        N_nez = [temp['nez'] for temp in data.N]
+        s = stations[i], data.glat.to_string(index=False), data.glon.to_string(index=False), N_nez[0]
         list.append(s)
-        ll = [data.glat.item(), data.glon.item()]
-        coord.append(ll)
-        p = [data.sza.item()]
-        param.append(p)
+        coord.append([data.glat.item(), data.glon.item()])
+        param.append([data.sza.item()])
 
     #print(coord)
     #print(param)
 
-    st = request.GET.get('coord').split(',')
-    new_coord = [float(s) for s in st]
-    kriging(coord, param, new_coord)
+    # st = request.GET.get('coord').split(',')
+    # new_coord = [float(s) for s in st]
+    # kriging(coord, param)
 
-    df = pd.DataFrame(list, columns=['station', 'latitude', 'longitude', 'SZA'])
+    df = pd.DataFrame(list, columns=['station', 'latitude', 'longitude', 'par'])
     #df.to_csv(f"C:\\Users\\alino\\PycharmProjects\\djangoProject\\static\\dataa.csv", index=False)
-    geo_json = to_geojson(df=df, lat='latitude', lon='longitude', properties=['station', 'SZA'])
-
+    geo_json = to_geojson(df=df, lat='latitude', lon='longitude', properties=['station', 'par'])
+    time.sleep(5)
+    print(datetime.now() - start_time)
     return JsonResponse(geo_json)
 
-def kriging(coord, param, new_coord):
+def kriging(request):
     import openturns as ot
     # Input points
     coordinates_train = coord
@@ -51,8 +53,22 @@ def kriging(coord, param, new_coord):
     # Fit
     inputDimension = 2
 
-    basis = ot.ConstantBasisFactory(inputDimension).build()
+    # basis = ot.ConstantBasisFactory(inputDimension).build()
+    # basis = ot.LinearBasisFactory(inputDimension).build()
+    # basis = ot.QuadraticBasisFactory(inputDimension).build()
+    basis = []
+
     covarianceModel = ot.SquaredExponential([1.] * inputDimension, [1.0])
+    # covarianceModel = ot.MaternModel([1.0] * inputDimension, 1.5)
+
+    # basis = ot.ConstantBasisFactory(inputDimension).build()
+    # covarianceModel = ot.SphericalModel(inputDimension)
+
+    # covarianceModel = ot.SquaredExponential([1.0])
+    # covarianceModel.setActiveParameter([])
+
+    # thetaInit = 1.0
+    # covariance = ot.GeneralizedExponential([thetaInit], 2.0)
 
     algo = ot.KrigingAlgorithm(coordinates_train, precipitation_train, covarianceModel, basis)
     algo.run()
@@ -62,9 +78,17 @@ def kriging(coord, param, new_coord):
     # Predict
     # coordinates = [15.70, 62.53]  # A new latitude/longitude pair
     # precipitation = krigingMetamodel(coordinates)
+    st = request.GET.get('coord').split(',')
+    new_coord = [float(s) for s in st]
+
     precipitation = krigingMetamodel(new_coord)
-    print(precipitation)
-    return (precipitation)
+
+
+    list = [('Новая точка', st[0], st[1], str(precipitation[0]))]
+    df = pd.DataFrame(list, columns=['station', 'latitude', 'longitude', 'par'])
+    geo_json = to_geojson(df=df, lat='latitude', lon='longitude', properties=['station', 'par'])
+
+    return JsonResponse(geo_json)
 
 def index(request):
     # if request.GET.get('submit'):
